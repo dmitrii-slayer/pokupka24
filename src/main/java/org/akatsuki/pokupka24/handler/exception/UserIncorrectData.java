e0446672bba94b22bfb62c74501fe7a5135f7099
Dmitrii Vartik <dekesite@mail.ru>
2024-05-26 09:09:47 +0300

    add exception handling

---
+++ src/main/java/org/akatsuki/pokupka24/handler/exception/UserIncorrectData.java
@@ -0,0 +1,12 @@
+package org.akatsuki.pokupka24.handler.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserIncorrectData {
+
+    private String info;
+
+}
