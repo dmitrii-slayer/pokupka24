a7e0499762a93715e876d5f91f0ff47c3f5eef3d
Dmitrii Vartik <dekesite@mail.ru>
2024-06-01 09:03:48 +0300

    teapot

--- src/main/java/org/akatsuki/pokupka24/handler/UserGlobalExceptionHandler.java
+++ src/main/java/org/akatsuki/pokupka24/handler/UserGlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.akatsuki.pokupka24.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.akatsuki.pokupka24.handler.exception.NoSuchUserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,23 +8,26 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Slf4j
 @ControllerAdvice
 public class UserGlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler
     public ResponseEntity<UserIncorrectData> handleException(NoSuchUserException exception) {
+        log.debug(exception.getMessage());
         UserIncorrectData data = new UserIncorrectData();
         data.setInfo(exception.getMessage());
         return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
     @ExceptionHandler
     public ResponseEntity<UserIncorrectData> handleException(Exception exception) {
+        log.error(exception.getMessage());
         UserIncorrectData data = new UserIncorrectData();
         data.setInfo(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(data, HttpStatus.I_AM_A_TEAPOT);
     }
 
 }
