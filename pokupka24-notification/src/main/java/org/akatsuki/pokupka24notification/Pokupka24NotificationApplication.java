b29918c8f7b72a9c5468ae06f136b48429cc2410
Dmitrii Vartik <dekesite@mail.ru>
2024-06-15 23:07:36 +0300

    add notification app

---
+++ pokupka24-notification/src/main/java/org/akatsuki/pokupka24notification/Pokupka24NotificationApplication.java
@@ -0,0 +1,13 @@
+package org.akatsuki.pokupka24notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pokupka24NotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pokupka24NotificationApplication.class, args);
+	}
+
+}
---
+++ pokupka24-notification/src/test/java/org/akatsuki/pokupka24notification/Pokupka24NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.akatsuki.pokupka24notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pokupka24NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
