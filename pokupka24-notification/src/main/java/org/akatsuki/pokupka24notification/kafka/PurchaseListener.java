21a8c0e494659a5f428fec3c1b4ef695fb9c0c69
Dmitrii Vartik <dekesite@mail.ru>
2024-06-16 00:05:26 +0300

    add kafka consumer

---
+++ pokupka24-notification/src/main/java/org/akatsuki/pokupka24notification/kafka/PurchaseListener.java
@@ -0,0 +1,18 @@
+package org.akatsuki.pokupka24notification.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PurchaseListener {
+
+    private static final Logger log = LoggerFactory.getLogger(PurchaseListener.class);
+
+    @KafkaListener(topics = "quickstart-events", groupId = "group1")
+    public void listen(String data) {
+        log.info("Вычитали сообщение: {}", data);
+    }
+
+}
