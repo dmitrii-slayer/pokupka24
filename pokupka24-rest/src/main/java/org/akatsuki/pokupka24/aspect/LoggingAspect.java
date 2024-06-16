cdfdcd0f1d02d107f6e2216d20f7e90092b85943
Dmitrii Vartik <dekesite@mail.ru>
2024-06-16 19:10:47 +0300

    parameterized execution time threshold for logging

--- pokupka24-rest/src/main/java/org/akatsuki/pokupka24/aspect/LoggingAspect.java
+++ pokupka24-rest/src/main/java/org/akatsuki/pokupka24/aspect/LoggingAspect.java
@@ -5,12 +5,16 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect
 @Component
 public class LoggingAspect {
+
+    @Value("${logging.executionTimeThreshold.controller}")
+    private int executionTimeThreshold;
 
     @Around("execution(* org.akatsuki.pokupka24.rest.controller.*.*(..))")
     public Object logSlowEndpointsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
@@ -20,10 +24,10 @@ public class LoggingAspect {
 
         long finish = System.currentTimeMillis();
 
-        long duration = finish - start;
-        if (duration > 100) {
+        long executionTime = finish - start;
+        if (executionTime > executionTimeThreshold) {
             MethodSignature method = (MethodSignature) proceedingJoinPoint.getSignature();
-            log.info("Execution took {} ms for {}.{}", duration, method.getDeclaringTypeName(), method.getName());
+            log.info("Execution took {} ms for {}.{}", executionTime, method.getDeclaringTypeName(), method.getName());
         }
         return targetMethodResult;
     }
