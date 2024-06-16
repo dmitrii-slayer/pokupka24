b111f0032e34aa9264190fda0a8cf07160342ba0
Dmitrii Vartik <dekesite@mail.ru>
2024-06-16 07:53:20 +0300

    add PurchaseDTO consumer handling

--- pokupka24-notification/src/main/java/org/akatsuki/pokupka24notification/kafka/KafkaConsumerConfig.java
+++ pokupka24-notification/src/main/java/org/akatsuki/pokupka24notification/kafka/KafkaConsumerConfig.java
@@ -1,13 +1,16 @@
 package org.akatsuki.pokupka24notification.kafka;
 
+import org.akatsuki.pokupka24.dto.PurchaseDTO;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,13 +23,24 @@ public class KafkaConsumerConfig {
     private String bootstrapServers;
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+    public ConsumerFactory<String, PurchaseDTO> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
                 bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
                 StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(),
+                new JsonDeserializer<>(PurchaseDTO.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PurchaseDTO> purchaseListener() {
+        ConcurrentKafkaListenerContainerFactory<
+                String, PurchaseDTO> factory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+
+        return factory;
     }
 }
--- pokupka24-notification/src/main/java/org/akatsuki/pokupka24notification/kafka/PurchaseConsumer.java
+++ pokupka24-notification/src/main/java/org/akatsuki/pokupka24notification/kafka/PurchaseConsumer.java
@@ -1,20 +1,22 @@
 package org.akatsuki.pokupka24notification.kafka;
 
+import org.akatsuki.pokupka24.dto.PurchaseDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
-@KafkaListener(topics = "${kafka.topic.purchases}")
 public class PurchaseConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(PurchaseConsumer.class);
 
-    @KafkaHandler
-    public void handle(String data) {
-        log.info("Вычитали сообщение: {}", data);
+    @KafkaListener(topics = "${kafka.topic.purchases}",
+            groupId = "${spring.kafka.consumer.group-id}",
+            containerFactory = "purchaseListener")
+    public void handle(@Payload PurchaseDTO purchase) {
+        log.info("Вычитали сообщение: {}", purchase);
     }
 
 }
